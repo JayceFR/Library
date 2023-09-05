f90e271342848746768e2cc22ba5d8cefc082c89
JayceFR <jaycejefferson.vicious@gmail.com>
2023-09-05 11:14:50 +0100

    Unique email address

--- Api/go.mod
+++ Api/go.mod
@@ -3,11 +3,11 @@ module github.com/JayceFR/library
 go 1.21.0
 
 require (
-	github.com/google/uuid v1.3.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/google/uuid v1.3.1
+	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-	gorm.io/driver/sqlite v1.5.3 // indirect
-	gorm.io/gorm v1.25.4 // indirect
+	gorm.io/driver/sqlite v1.5.3
+	gorm.io/gorm v1.25.4
 )
--- Api/src/api/handlers/accountHandler.go
+++ Api/src/api/handlers/accountHandler.go
@@ -30,15 +30,25 @@ type CreateAccount struct {
 	Password  string `json:"password"`
 }
 
+const null_uuid = "00000000-0000-0000-0000-000000000000"
+
 func (s *ApiHandler) handleCreateAccount(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	createAccount := CreateAccount{}
 	err := json.NewDecoder(r.Body).Decode(&createAccount)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	account := s.NewAccount(createAccount.FirstName, createAccount.Email, createAccount.Password)
-	s.db.Create(account)
-	return s.WriteJson(w, http.StatusOK, createAccount)
+	var check_account Account
+	s.db.First(&check_account, "email = ?", createAccount.Email)
+	if check_account.ID.String() == null_uuid {
+		fmt.Println("Email is not Found")
+		account := s.NewAccount(createAccount.FirstName, createAccount.Email, createAccount.Password)
+		s.db.Create(account)
+		return s.WriteJson(w, http.StatusOK, createAccount)
+	} else {
+		return s.WriteJson(w, http.StatusBadRequest, "Found another user with the same email id")
+	}
+
 }
 
 func (s *ApiHandler) handleDeleteAccount(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
