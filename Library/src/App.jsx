3a1405e026793185551f27557e070464333e981f
JayceFR <jaycejefferson.vicious@gmail.com>
2023-10-19 20:39:55 +0100

    Updated regex

--- Library/src/App.jsx
+++ Library/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [can_submit, setCan_submit] = useState(false)
 
   const email_regex = new RegExp(/^[a-z0-9A-Z]{2,}@..*\.com$/gm)
-  const password_regex = new RegExp(/\w/gm)
+  const password_regex = new RegExp(/\w{8,}/gm)
 
   function submit(e){
     e.preventDefault()
@@ -21,35 +21,7 @@ function App() {
     get_user_id()
   }
 
-  /*useEffect( () => {
-    const fetchdata = async () => {
-      const result = await fetch("https://library.mysticjayce.repl.co/user_id")
-      console.log(result)
-    }
-    fetchdata()
-  }, [])*/
-
   const get_user_id = async () => {
-    //const base_url = 'https://library.mysticjayce.repl.co/'
-    // const base_url = 'http://127.0.0.1:5000/'
-    // const result = await fetch(base_url + 'user_id')
-    // const json_result = await result.json()
-    // console.log(json_result['id'])
-    // const put_url = base_url + "user/" + json_result['id']   
-    // console.log(put_url)
-    // const user_data = {
-    //   "email" : email,
-    //   "password" : password
-    // }
-    // const result2 = await fetch(put_url, {
-    //   method: 'PUT', 
-    //   headers: {
-    //     'Content-Type' : 'application/json'
-    //   },
-    //   body: JSON.stringify(user_data)
-    // })
-    // const json_result2 = await result2.json()
-    // console.log(json_result2)
 
     const base_url = "http://localhost:3000/account"
     const user_data = {
@@ -94,7 +66,7 @@ function App() {
 
   return (
     <>
-     <p>Login Page</p>
+     <p>Sign Up Page</p>
      <br></br>
      <form onSubmit={submit}>
       <label>Name</label>
